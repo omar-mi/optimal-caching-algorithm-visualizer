84cd65fd4ccea86f276af701be3a6ac391bd43c5
Omar Mohamed <42996937+omar-mi@users.noreply.github.com>
2025-12-17 21:23:51 +0200

    Refactor cache input to accept user-defined elements
    Refactor cache and access sequence input handling, removing random generation and allowing user-defined elements. Update UI labels and drawing functions accordingly. Draw arrow above the to be replaced element before removing it

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from tkinter import *
 from tkinter import ttk, messagebox
-import random
-import string
 
 root = Tk()
 root.title("Optimal Caching Algorithm Visualizer")
@@ -13,6 +11,7 @@ canvas = None
 RECT_SIZE = 75
 MAX_RECTS = root.winfo_screenwidth() // RECT_SIZE - 2
 
+to_replace = None
 access_sequence = []
 cache = {}
 driver_index = 0
@@ -23,18 +22,6 @@ hit_label = None
 
 miss_count = 0
 miss_label = None
-
-def random_char_list(n, has_duplicates=True):
-    if has_duplicates:
-        return [random.choice(string.ascii_uppercase) for _ in range(n)]
-    else:
-        return random.sample(string.ascii_uppercase, n)
-
-def random_char_dict(n, has_duplicates=True):
-    if has_duplicates:
-        return [random.choice(string.ascii_uppercase) for _ in range(n)]
-    else:
-        return {x: 0 for x in  random.sample(string.ascii_uppercase, n)}
     
 def draw_cache_list(cache):
     """Draw the cache boxes centered at the top."""
@@ -42,6 +29,10 @@ def draw_cache_list(cache):
     x_offset = root.winfo_screenwidth() // 2 - (len(cache) * RECT_SIZE) // 2
     y_offset = 175
 
+    canvas.create_text(x_offset - 50, y_offset + RECT_SIZE / 2,
+                       text="Cache:", fill="white", font=("Helvetica", 18))
+    
+    temp = None
     for idx, c in enumerate(cache):
         x1 = x_offset + idx * RECT_SIZE
         x2 = x1 + RECT_SIZE
@@ -49,9 +40,12 @@ def draw_cache_list(cache):
         y2 = y1 + RECT_SIZE
         canvas.create_rectangle(x1, y1, x2, y2, outline="white")
         canvas.create_text((x1 + x2) / 2, (y1 + y2) / 2, text=c, fill="white", font=("Helvetica", 20))
-
-        canvas.create_text(x_offset - 50, y_offset + RECT_SIZE / 2,
-                           text="Cache:", fill="white", font=("Helvetica", 18))
+        # draw cache element's LRU value above it
+        canvas.create_text((x1 + x2) / 2, y1 - 15, text=str(cache[c]), fill="yellow", font=("Helvetica", 14))
+        if to_replace and c == to_replace[0]:
+            temp = (x1, y1, x2, y2)
+    if temp:
+        canvas.create_rectangle(temp[0], temp[1], temp[2], temp[3], outline="red", width=3)
 
 
 def draw_access_sequence(access):
@@ -60,6 +54,8 @@ def draw_access_sequence(access):
     x_offset = root.winfo_screenwidth() // 2 - (len(access) * RECT_SIZE) // 2
     y_offset = 350
 
+    canvas.create_text(x_offset - 100, y_offset + RECT_SIZE / 2,
+                       text="Access Sequence:", fill="white", font=("Helvetica", 18))
     for idx, c in enumerate(access):
         x1 = x_offset + idx * RECT_SIZE
         x2 = x1 + RECT_SIZE
@@ -68,15 +64,13 @@ def draw_access_sequence(access):
         canvas.create_rectangle(x1, y1, x2, y2, outline="lightblue")
         canvas.create_text((x1 + x2) / 2, (y1 + y2) / 2, text=c, fill="lightblue", font=("Helvetica", 20))
 
-        canvas.create_text(x_offset - 53, y_offset + RECT_SIZE / 2,
-                           text="Visa hh:", fill="white", font=("Helvetica", 18))  # cache wla visa (for the illiterate)
-
 def draw_driver(index):
     """Draw the arrow pointing to the current element in access sequence."""
     global canvas, access_sequence, driver_arrow
     global hit_count, hit_label
     global miss_count, miss_label
     global cache
+    global to_replace
 
     if driver_arrow:
         canvas.delete(driver_arrow)
@@ -103,12 +97,11 @@ def draw_driver(index):
 
         # TODO: Apply OPTIMAL ALGORITHM to find the victim
         LRU_val = min(cache.values())
+
         # o(n)
         for i in cache:
             if(cache[i] == LRU_val):
-                del cache[i]
-                cache[access_sequence[index]] = 0
-                print(cache)
+                to_replace = (i, access_sequence[index])
                 break
         # Redraw updated cache
         canvas.delete("all")
@@ -120,9 +113,16 @@ def draw_driver(index):
 
 def next_step():
     """Move the driver arrow 1 step to the right."""
-    global driver_index, access_sequence
+    global driver_index, access_sequence, to_replace
 
-    if driver_index < len(access_sequence):
+    if to_replace:
+        del cache[to_replace[0]]
+        cache[to_replace[1]] = 0
+        to_replace = None
+        canvas.delete("all")
+        draw_cache_list(cache)
+        draw_access_sequence(access_sequence)
+    elif driver_index < len(access_sequence):
         driver_index += 1
         draw_driver(driver_index)
 
@@ -138,28 +138,37 @@ def draw_lists(cache, access_sequence):
 
 
 
-cache_input_label = ttk.Label(root, text="Enter cache size:", font=("Helvetica", 26))
+cache_input_label = ttk.Label(root, text="Enter cache elements:", font=("Helvetica", 26))
 cache_input_label.pack(pady=(150, 20))
 cache_input = ttk.Entry(root, width=30)
 cache_input.pack(pady=(5, 0))
 
-access_input_label = ttk.Label(root, text="Enter number access sequence size:", font=("Helvetica", 26))
+access_input_label = ttk.Label(root, text="Enter your access sequence:", font=("Helvetica", 26))
 access_input_label.pack(pady=(40, 20))
 access_input = ttk.Entry(root, width=30)
 access_input.pack(pady=(5, 20))
 
 
 def on_start_viz():
-    global cache, access_sequence, driver_index, hit_label, miss_label, miss_count
+    global cache, access_sequence, cache_elements, access_sequence_elements
+    global driver_index, hit_label, miss_label, miss_count
 
-    cache_size = cache_input.get()
+    cache_elements = cache_input.get()
     try:
-        cache_size = int(cache_size)
-        if cache_size <= 0 or cache_size > MAX_RECTS:
+        if len(cache_elements) <= 0 or len(cache_elements) > MAX_RECTS:
             raise ValueError
     except ValueError:
         messagebox.showerror("Invalid Input",
-                             f"Enter a number between 1 and {MAX_RECTS} for cache size.")
+                             f"Limit cache to between 1 and {MAX_RECTS} elements")
+        return
+
+    access_sequence_elements = access_input.get()
+    try:
+        if len(access_sequence_elements) <= 0 or len(access_sequence_elements) > MAX_RECTS:
+            raise ValueError
+    except ValueError:
+        messagebox.showerror("Invalid Input",
+                             f"Limit access sequence to between 1 and {MAX_RECTS} elements")
         return
 
     # Create miss/hit labels
@@ -169,16 +178,6 @@ def on_start_viz():
     hit_label = ttk.Label(root, text=f"Hits: {hit_count}", font=("Helvetica", 20))
     hit_label.pack(pady=(0, 10))
 
-    num_requests = access_input.get()
-    try:
-        num_requests = int(num_requests)
-        if num_requests <= 0 or num_requests > MAX_RECTS:
-            raise ValueError
-    except ValueError:
-        messagebox.showerror("Invalid Input",
-                             f"Enter a number between 1 and {MAX_RECTS} for access requests.")
-        return
-
     # Remove init UI
     welcome.destroy()
     cache_input_label.destroy()
@@ -186,11 +185,10 @@ def on_start_viz():
     access_input_label.destroy()
     access_input.destroy()
     start_button.destroy()
-    omar_label.destroy()
 
     # Init simulation
-    cache = random_char_dict(cache_size, has_duplicates=False)
-    access_sequence = random_char_list(num_requests, has_duplicates=True)
+    cache = {c: 0 for c in cache_elements}
+    access_sequence = list(access_sequence_elements)
     driver_index = -1
 
     draw_lists(cache, access_sequence)
@@ -203,8 +201,5 @@ start_button = ttk.Button(root, text="Start Visualization", command=on_start_viz
 start_button.pack(pady=(20, 60))
 style = ttk.Style()
 style.configure("Big.TButton", font=("Helvetica", 14))
-
-omar_label = ttk.Label(root, text="I love Omar ❤️", font=("Helvetica", 26))
-omar_label.pack(pady=20)  # dont delete pls :(
 
 root.mainloop()
